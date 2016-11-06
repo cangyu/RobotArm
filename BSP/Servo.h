41b122861e7947259c54f93eb37f6de9c2ef277c
Yu Cang <yhcy1993@gmail.com>
2016-11-06 13:55:17 +0800

    add s-curve

--- BSP/Servo.h
+++ BSP/Servo.h
@@ -1,39 +1,46 @@
 #ifndef _SERVO_H_
 #define _SERVO_H_
 
+/* Includes */
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
 #include <math.h>
+#include <stdlib.h>
 #include <ucos_ii.h>
 #include "stm32f10x.h"
 #include "RobotArmCtrlBoard.h"
+#include "s_curve.h"
 
+/* Defines */
 #define ROBOTIC_ARM_NUM 10
 #define SERVOS_USED 0x017F
 
+/* Global Variables */
 extern char* servo_name[ROBOTIC_ARM_NUM];
-extern float cur_angle[ROBOTIC_ARM_NUM];
-extern float target_angle[ROBOTIC_ARM_NUM];
-extern float ang_min[ROBOTIC_ARM_NUM];
-extern float ang_max[ROBOTIC_ARM_NUM];
-extern int cur_pwm[ROBOTIC_ARM_NUM];
-extern int target_pwm[ROBOTIC_ARM_NUM];
-extern int pwm_min[ROBOTIC_ARM_NUM];
-extern int pwm_max[ROBOTIC_ARM_NUM];
-extern int gap[ROBOTIC_ARM_NUM];
-extern int pwm_resolution[ROBOTIC_ARM_NUM];
 extern OS_EVENT *SemApply[ROBOTIC_ARM_NUM];
 extern char* sem_name[ROBOTIC_ARM_NUM];
 
+extern float cur_angle[ROBOTIC_ARM_NUM];
+extern float target_angle[ROBOTIC_ARM_NUM];
+extern const float ang_min[ROBOTIC_ARM_NUM];
+extern const float ang_max[ROBOTIC_ARM_NUM];
+extern int cur_pwm[ROBOTIC_ARM_NUM];
+extern int target_pwm[ROBOTIC_ARM_NUM];
+extern const int pwm_min[ROBOTIC_ARM_NUM];
+extern const int pwm_max[ROBOTIC_ARM_NUM];
+extern const int pwm_resolution[ROBOTIC_ARM_NUM];
+
+/* Function Prototypes */
 int min(int a, int b);
 int max(int a, int b);
 int angle2pwm(int i, float _ang);
 float pwm2ang(int i, int _pwm);
 void move(int i);
-void advance_one(int i);
+void advance_to_next(int i);
 void pos_update(int index);
 void Servo_InitMove(void);
 void Servo_Run(void);
+void optimized_move(int index);
 
 #endif
--- Project/IAR/settings/RobotArm.Release.cspy.bat
+++ Project/IAR/settings/RobotArm.Release.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\CangYu\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.general.xcl" --backend -f "C:\Users\CangYu\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.general.xcl" --backend -f "C:\Users\Administrator\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\CangYu\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\CangYu\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Administrator\Desktop\RobotArm\Project\IAR\settings\RobotArm.Release.driver.xcl" 
 
 @echo off 
 :end
